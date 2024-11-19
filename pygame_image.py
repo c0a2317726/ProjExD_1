8d0db08023bd2bf1a71c51030227cf320db3bf5e
higashi <c0a2317726@edu.teu.ac.jp>
2024-11-19 15:37:12 +0900

    練習2：こうかとん画像の描画

--- pygame_image.py
+++ pygame_image.py
@@ -10,12 +10,14 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/3.png")#こうかとん画像の読み込みsurfaceを作成
+    kk_img = pg.transform.flip(kk_img, True, False) #こうかとん左右反転
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [100, 0]) # screen surfaceに背景画像surfaceを貼り付ける
+        screen.blit(bg_img, [0, 0]) # screen surfaceに背景画像surfaceを貼り付ける
         pg.display.update()
         tmr += 1        
         clock.tick(10)
